d4fb5b96b6a5a5ef223153aab186fb78f38d5e92
Ellen Shapiro <designatednerd@gmail.com>
2019-07-08 09:12:26 +0100

    cleaner logging

--- Scripty/Sources/Scripty/main.swift
+++ Scripty/Sources/Scripty/main.swift
@@ -1,15 +1,15 @@
 // drop the first argument, which is the path to the script
 let args = CommandLine.arguments.dropFirst()
-print("Args: \(args)")
+print("Arguments: \(args)")
 
 let plistFile = try SourceFolders.appRoot.file(named: "Environment.plist")
 
 if args.contains("--prebuild") {
-    // Run pre-build tasks
+    print("Run pre-build tasks")
     try EnvironmentUpdater.updateEnvironment(file: plistFile)
 }
 
 if args.contains("--postbuild") {
-    // Run post-build tasks
+    print("Run post-build tasks")
     try EnvironmentUpdater.resetEnvironment(file: plistFile)
 }
